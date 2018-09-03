444118ba9071a420dd3cf8aca0a9c27a3898be1f
Plasterfour <42197092+Plasterfour@users.noreply.github.com>
2018-09-02 23:09:35 -0500

    Add files via upload

---
+++ Encriptar.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	char Palabra[256];
+	char clave[256];
+	int i = 0;
+	printf("\nIntroduzca palabra o frase a cifrar:\n");
+	fgets(Palabra,255,stdin);
+	printf("\nLa frase es:\n");
+	for  (i = 0;Palabra[i]!='\0';i++)
+		printf("%c ",Palabra[i]);
+	
+	printf("\nIngrese la contraseña:\n");
+		fgets(clave,255,stdin);
+		printf ("\nFrase encriptada:\n");
+	for  (i = 0; Palabra[i]!='\0';i++)
+	{
+		Palabra[i]+=clave[i];
+		printf("%c ",Palabra[i]);
+	}
+	printf("\n\nFrase desencriptada:\n");
+	for  (i = 0; Palabra[i]!='\0';i++)
+	{
+		Palabra[i]-=clave[i];
+		printf("%c ",Palabra[i]);
+	}
+	return 0;
+}
